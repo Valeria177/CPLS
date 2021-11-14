38d59630cdec7f7c4b3e864fbca8fb6b5df7195e
Valeria177 <levchenkovalerya@yandex.ru>
2021-11-14 18:18:29 +0300

    Изменение стиля экранов

---
+++ app/src/main/java/com/example/cptestls/GoLogin.java
@@ -0,0 +1,14 @@
+package com.example.cptestls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_login);
+    }
+}
